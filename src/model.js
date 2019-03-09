09e1864a6f8e3c0d72235a225be1b0e9a9b8281c
Carlo Ferrer <thecarloferrer@gmail.com>
2019-03-08 21:52:59 -0500

    Issue #6: Explicitly define timestamp and formatting (#10)
    - Correct previously unobserved issue of timestamp never updating.
    - Allow Node version agnostic date and time formatting.

--- src/model.js
+++ src/model.js
@@ -2,43 +2,30 @@ import mongoose from 'mongoose';
 
 /*
 ** Schema:
-** index | date | timestamp | task | detail
+** index | date | time | task | detail
 */
 
-// This will need to change if in a different timezone.
-const now = new Date().toLocaleString('en-CA', { timeZone: 'America/New_York' });
-const date = now.slice(0, 10);
-const timestamp = now.slice(12);
-
 const schema = mongoose.Schema({
-  // index: {
-  //   type: Number,
-  //   required: true
-  // },
   date: {
     type: String,
     required: true,
-    default: date
   },
-  timestamp: {
+  time: {
     type: String,
     required: true,
-    default: timestamp
   },
   task: {
     type: String,
-    required: true
+    required: true,
   },
   detail: {
     type: String,
-    required: false
+    required: false,
   }
 });
 
 const lineItem = mongoose.model('line-item', schema);
 
-lineItem.get = (cb, limit) => {
-  lineItem.find(cb).limit(limit);
-};
+lineItem.get = (cb, limit) => lineItem.find(cb).limit(limit);
 
 export default lineItem;
